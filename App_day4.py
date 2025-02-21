6d2665dcbc5aa34dad6a9b5af74fc0613c8346e0
ramalho-cmd <rapharam96@gmail.com>
2025-02-21 14:52:04 -0300

    Deploy 2

--- App_day4.py
+++ App_day4.py
@@ -1,3 +1,5 @@
+
+
 import streamlit as st
 import pandas as pd
 from datetime import datetime, timedelta
@@ -5,18 +7,18 @@ from datetime import datetime, timedelta
 @st.cache_data
 def carregar_dados():
     df = pd.read_csv(r'C:\Users\pauli\OneDrive\Documentos\GitHub\StreamlitApp\StreamlitApp\base_dados.csv')
-    return df
-print(carregar_dados())    
-
-df = carregar_dados()
+    return df 
 
 st.write("""# Busque as musicas mais tocadas na época:""")
 st.write("""## É só informar o mês e o ano!!!""")
 mes = st.selectbox('Mês:',['1','2','3','4','5','6','7','8','9','10','11','12'])
 ano = st.text_input('Ano:')
 busca = mes + "/" + ano
+
+df = carregar_dados()
 dft = df[df['Data_abr'] == busca]
 dft = dft[['Titulo','Autor','Spotify_link']]
 dft = dft.reset_index(drop=True)
 dft.index = dft.index+1
+
 st.write(dft)
